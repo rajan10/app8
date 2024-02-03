d91208b57f9c5da9536c57ab0a4d2a73d9772a73
Rajan Gauchan <hirajan10@hotmail.com>
2024-02-03 01:00:19 -0500

    Add Angular Routing to create page navigations that will map the route(url) with the corresponding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule, Routes } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
+
+var myRoutes:Routes = [
+  {path:"", component:HomeComponent},
+  {path:"home", component:HomeComponent},
+  {path:"about",component: AboutComponent},
+  {path:"contact", component: ContactComponent},
+  {path:"services", component: ServicesComponent}
+];
+
+var myRoutes2 = RouterModule.forRoot(myRoutes);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, FormsModule,myRoutes2
   ],
   providers: [],
   bootstrap: [AppComponent]
